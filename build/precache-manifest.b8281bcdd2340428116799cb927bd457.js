690d737628a784119d94c3ff68544f86f11113f3
asriidev-gh <asriidev@gmail.com>
2020-07-18 01:12:14 +0800

    Updates

---
+++ build/precache-manifest.b8281bcdd2340428116799cb927bd457.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "164be475d846f0f956d41ea6d33a6e96",
+    "url": "/asriidev-gh/index.html"
+  },
+  {
+    "revision": "3aed75b39409e98872db",
+    "url": "/asriidev-gh/static/js/2.fc5d49dc.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/asriidev-gh/static/js/2.fc5d49dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54505e465e8f40d86cc4",
+    "url": "/asriidev-gh/static/js/main.53b27d4d.chunk.js"
+  },
+  {
+    "revision": "c15919f501d9a393cbb1",
+    "url": "/asriidev-gh/static/js/runtime-main.35e2d8be.js"
+  }
+]);
